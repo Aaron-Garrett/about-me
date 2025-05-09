7bfdba1cb27ccfc1c0254eacdc30795aa00df61c
IAmBatJew <aamigarr@gmail.com>
2025-05-08 20:37:52 -0400

    Got the landing part of the landing page working

---
+++ testscript.js
@@ -0,0 +1,7 @@
+const toggleNav = () => {
+    setTimeout(() => {
+        document.body.classList.add('open');
+        document.querySelector('.flag-string').classList.add('risen');
+        document.querySelector('#nav-toggle').classList.add('open');
+    }, 500);
+};
